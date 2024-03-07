ec8d04880f4baaaf5d0b161e21e8b90a58c9a94b
CsengeOnCode <161061835+CsengeOnCode@users.noreply.github.com>
2024-03-07 21:03:17 +0000

    Injected the forecast HTML from Javascript

--- src/script.js
+++ src/script.js
@@ -54,7 +54,33 @@ function handleSearchCity(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+     <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature-max"> 15º </div>
+      <div class="weather-forecast-temperature-min">9º</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchCity);
 
 searchCity("Lisbon");
+
+displayForecast();
